2bca3f9e8c1f060985b8d736dd70b21d612fa34f
xukaixuan121 <2046915672@qq.com>
2023-06-29 17:32:35 +0800

    again test

--- test.c
+++ test.c
@@ -4,6 +4,9 @@ int main()
 {
     printf("Hello World!\n");
     printf("你好世界！\n");
-    system("pause");    // 防止运行后自动退出，需头文件stdlib.h
+    system("pause");            // 防止运行后自动退出，需头文件stdlib.h
+
     return 0;
+
 }
+
